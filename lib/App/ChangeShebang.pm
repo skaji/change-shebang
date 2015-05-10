87235977abe47eb1ec5d386548b4ce77e74704d1
Shoichi Kaji <skaji@cpan.org>
2015-05-10 15:25:43 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index aa190bf..6e524bb 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension App-ChangeShebang
    
     {{$NEXT}}
    
    +0.04 2015-05-10T06:24:57Z
    +
    +    - remove unnecessary "--" for exec perl
    +    - remove original shebang as well as "not running under some shell" stuff
    +
     0.03 2014-07-06T05:34:18Z
    
         - to make sure rename() properly works, make a tempfile in the same directory

--- lib/App/ChangeShebang.pm
+++ lib/App/ChangeShebang.pm
@@ -9,7 +9,7 @@ use File::Basename 'dirname';
 use File::Temp 'tempfile';
 sub prompt { ExtUtils::MakeMaker::prompt(@_) }
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub new {
     my $class = shift;
